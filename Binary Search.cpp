cd5ea61151e1660d2feaee3090310122fa814238
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-20 09:55:29 +0600

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,32 +1,42 @@
-#include <bits/stdc++.h>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/#include <bits/stdc++.h>
 using namespace std;
-
-int bS(int arr[], int l, int r, int x)
+int bs(int ar[],int l,int r,int x)
 {
-    if (r >= l) {
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == x)
+    if(r>=l)
+    {
+        int mid=l+(r-l)/2;
+
+
+        if(ar[mid]==x)
             return mid;
-        if (arr[mid] > x)
-            return bS(arr, l, mid - 1, x);
-        return bS(arr, mid + 1, r, x);
+
+        if(ar[mid]>x)\
+            return bs(ar,l,mid-1,x);
+
+        return bs(ar, mid+1,r,x);
     }
     return -1;
 }
 int main(void)
 {
-    int e,x;
-    cin>>e;
-    int arr[e];
-    for(int i=0; i<e; i++)
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0; i<n; i++)
     {
-        cin>>arr[i];
+        cin>>ar[i];
     }
-    cout<<"Enter element you want to search : ";
+    int x;
     cin>>x;
-    int n = sizeof(arr);
-    int result = bS(arr, 0, n - 1, x);
-    (result == -1) ? cout << "Element is not present in array"
-                   : cout << "Element is present at index " << result;
+    int l=sizeof(ar)/sizeof(ar[0]);
+    int result =bs(ar,0,l-1,x);
+    (result==-1)?cout<<"Element is not present here"
+                     : cout<<"Element is present here index\n"<<result;
     return 0;
 }
+

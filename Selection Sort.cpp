9c5155aa65a0fcb480ab9f0f7eecb504bbfeadc6
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-26 22:22:25 +0600

    Create Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,37 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++)
+	{
+		cin>>arr[i];
+	}
+
+	for(int i=0; i<n; i++)
+	{
+		for(int j=i+1; j<n; j++)
+		{
+			if(arr[j]<arr[i])
+			{
+				int temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+			}
+
+		}
+	}
+	for(int i=0; i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+
+}

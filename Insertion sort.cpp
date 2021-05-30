1bd1df1aa3d5b1557eb070a15ad399b84559568d
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-30 11:07:38 +0600

    Update Insertion sort.cpp

--- Insertion sort.cpp
+++ Insertion sort.cpp
@@ -6,22 +6,30 @@
 */
 #include<bits/stdc++.h>
 using namespace std;
-int a[100];
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
     {
-        int n;
-        cin>>n;
-        for(int i=0;i<2*n;i++) cin>>a[i];
-        sort(a,a+2*n);
-        for(int i=0;i<2*n-1;i++)
-        {
-            if(i&1 && a[i]<a[i+1]) swap(a[i],a[i+1]);
-        }
-        for(int i=0;i<2*n;i++) cout<<a[i]<<' ';
-        cout<<endl;
+        cin>>arr[i];
     }
+
+    for(int i=1; i<n; i++)
+    {
+        int crrnt=arr[i];
+        int j=i-1;
+        while(arr[j]>crrnt && j>=0)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=crrnt;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }cout<<endl;
 }
